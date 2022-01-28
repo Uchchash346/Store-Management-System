1d884207585824a9f7b5328f835c0ab571babe5f
Uchchash346 <mru416@gmail.com>
2022-01-28 16:51:19 +0600

    create order medicine quantity part complete

--- src/components/Sales Executive/CreateOrder/CreateOrder.js
+++ src/components/Sales Executive/CreateOrder/CreateOrder.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import './CreateOrder.css'
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const CreateOrder = () => {
     return (
-        <div>
-            <h1 className="text-center text-3xl">Create Order</h1>
-            <h2 className="text-center text-3xl">This is create order page</h2>
+        <div className="create-order-section pt-28">
+            <h2 className="text-center text-4xl create-order-heading">Create Order</h2>
+            <br />
+            <div className="container pt-5">
+                <div className="row">
+                    <div className="col-4">
+                        <p className="text-lg">Medicine</p>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <input
+                                    type="text"
+                                    className="form-control text-gray input-section"
+                                    aria-label="Sizing example input"
+                                    aria-describedby="inputGroup-sizing-sm"
+                                    placeholder="Medicine Name"
+                                />
+                            </div>
+                            <div className="col-sm-4">
+                                <input
+                                    type="number"
+                                    className="form-control text-gray input-section"
+                                    aria-label="Sizing example input"
+                                    aria-describedby="inputGroup-sizing-sm"
+                                    placeholder="Quantity"
+                                    value="0"
+                                />
+                            </div>
+                            <div className="col-sm-2 pt-1 pl-4 add-circle-button"><AddCircleIcon /></div>
+                        </div>
+                    </div>
+                    <div className="col-8">col-8</div>
+                </div>
+            </div>
         </div>
     );
 };
